c32747ee3af26bce6ab3e30c504a4c123665abc3
FiReBrO42 <sam1246747@hotmail.com>
2023-03-03 02:52:52 +0800

    git add

---
+++ src/views/front/ProductsView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>產品列表</div>
+  <table class="table">
+    <tbody>
+      <tr v-for="product in products" :key="product.id">
+        <td>{{ product.title }}</td>
+        <td><img :src="product.imageUrl" width="200" alt="" /></td>
+        <td>
+          <router-link
+            :to="`product/${product.id}`"
+            class="btn btn-outline-secondary"
+            >查看產品內容</router-link
+          >
+          <button
+            type="button"
+            class="btn btn-outline-danger"
+            @click="addToCart(product.id)"
+          >
+            加入購物車
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+const { VITE_APP_URL, VITE_APP_PATH } = import.meta.env
+
+export default {
+  data () {
+    return {
+      products: []
+    }
+  },
+  components: {
+    RouterLink
+  },
+  methods: {
+    // 取得產品API
+    getProducts () {
+      this.$http
+        .get(`${VITE_APP_URL}/v2/api/${VITE_APP_PATH}/products/all`)
+        .then((res) => {
+          this.products = res.data.products
+          console.log('產品列表', res.data.products)
+        })
+    },
+    // 加入購物車API
+    addToCart (id) {
+      // 建立要帶入的資料
+      const data = {
+        product_id: id,
+        qty: 1
+      }
+      this.$http
+        .post(`${VITE_APP_URL}/v2/api/${VITE_APP_PATH}/cart`, { data })
+        .then((res) => {
+          console.log(res.data.message)
+        })
+    }
+  },
+  mounted () {
+    this.getProducts()
+  }
+}
+</script>
